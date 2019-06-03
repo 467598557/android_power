defa094655428c0dddd8729cd6c90dd685037753
yunlou <yunlou@bcbook.cn>
2019-06-03 15:41:14 +0800

    添加惠头条的app文件，还没有处理过。

---
+++ app/src/main/java/com/accessibility/utils/apps/HuiTouTiao.java
@@ -0,0 +1,72 @@
+package com.accessibility.utils.apps;
+
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.accessibility.utils.AppInfo;
+import com.accessibility.utils.OperatorHelper;
+
+import java.util.List;
+
+// #TODO 待处理
+public class HuiTouTiao extends AppInfo {
+    public HuiTouTiao() {
+        this.packageName = "com.sohu.infonews";
+        this.startComponent = "com.sohu.quicknews.splashModel.activity.SplashActivity";
+        this.mainComponent = "com.sohu.quicknews.homeModel.activity.HomeActivity";
+    }
+
+    @Override
+    public String getListViewId() {
+        return "";
+    }
+
+    @Override
+    public AccessibilityNodeInfo getArticleSpecialViewById(OperatorHelper operatorHelper) {
+        List<AccessibilityNodeInfo> nodeList = operatorHelper.findNodesById("com.sohu.infonews:id/btm_divider");
+        AccessibilityNodeInfo node;
+        for(int i=1, len=nodeList.size(); i<len; i++) {
+            node = nodeList.get(i).getParent();
+            if(node.findAccessibilityNodeInfosByViewId("com.sohu.infonews:id/left_guess_tv").size() == 0) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public AccessibilityNodeInfo getVideoSpecialViewById(OperatorHelper operatorHelper) {
+        return getArticleSpecialViewById(operatorHelper);
+    }
+
+    @Override
+    public String getArticlePageContentViewId() {
+        return null;
+    }
+
+    @Override
+    public String getVideoPageContentVideoId() {
+        return null;
+    }
+
+    @Override
+    public boolean doSomething(OperatorHelper operatorHelper) {
+        AccessibilityNodeInfo root = operatorHelper.getRootNodeInfo();
+        if(null == root) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void doSomethingInDetailPage(OperatorHelper operatorHelper) {
+        AccessibilityNodeInfo root = operatorHelper.getRootNodeInfo();
+        if(null == root) {
+            return;
+        }
+
+        List<AccessibilityNodeInfo> nodeList;
+        AccessibilityNodeInfo node;
+    }
+}
