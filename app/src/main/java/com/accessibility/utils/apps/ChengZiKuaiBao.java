aa7626e93f6d09ea796dff6af523420ecaf51c0b
yunlou <yunlou@bcbook.cn>
2019-06-14 17:13:02 +0800

    优化橙子快报对于视频红包的获取

--- app/src/main/java/com/accessibility/utils/apps/ChengZiKuaiBao.java
+++ app/src/main/java/com/accessibility/utils/apps/ChengZiKuaiBao.java
@@ -86,9 +86,14 @@ public class ChengZiKuaiBao extends AppInfo {
             return;
         }
 
+        List<AccessibilityNodeInfo> nodeList = root.findAccessibilityNodeInfosByViewId("com.quyu.youliao:id/iv_click_text");
+        if(nodeList.size() > 0) {
+            operatorHelper.performClickActionByNodeListFirstChild(root.findAccessibilityNodeInfosByViewId("com.quyu.youliao:id/iv_redbag"));
+        }
+
         // 查看更多  com.quyu.youliao:id/ll_expand
         if(operatorHelper.runningCount % 2 == 0 && operatorHelper.runningCount % 3 != 0) {
-            List<AccessibilityNodeInfo> nodeList = root.findAccessibilityNodeInfosByViewId("com.quyu.youliao:id/ll_expand");
+            nodeList = root.findAccessibilityNodeInfosByViewId("com.quyu.youliao:id/ll_expand");
             if(nodeList.size() > 0) {
                 AccessibilityNodeInfo node = nodeList.get(0);
                 if(node.getText().toString().indexOf("点击查看全文") >= 0) {
