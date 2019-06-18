7b0a0e24698f93f34e197d4fd07532e5642964a4
yunlou <yunlou@bcbook.cn>
2019-06-18 08:23:09 +0800

    优化

--- app/src/main/java/com/accessibility/demo/WebViewNative.java
+++ app/src/main/java/com/accessibility/demo/WebViewNative.java
@@ -3,19 +3,22 @@ package com.accessibility.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.accessibility.R;
 
-public class WebViewNative extends Activity {
+public class WebViewNative extends Activity implements View.OnClickListener {
+    WebView mWebview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview_native);
 
-        WebView webview = findViewById(R.id.show_webview_wv);
-        webview.setWebViewClient(new WebViewClient(){
+        mWebview = findViewById(R.id.show_webview_wv);
+        mWebview.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
@@ -23,6 +26,30 @@ public class WebViewNative extends Activity {
             }
         });
 
-        webview.loadUrl("http://www.baidu.com");
+        mWebview.loadUrl("http://www.baidu.com");
+
+        findViewById(R.id.webview_btn1).setOnClickListener(this);
+        findViewById(R.id.webview_btn2).setOnClickListener(this);
+        findViewById(R.id.webview_btn3).setOnClickListener(this);
+        findViewById(R.id.webview_btn4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int id = v.getId();
+        switch (id) {
+            case R.id.webview_btn1:
+                mWebview.loadUrl("http://www.baidu.com");
+                break;
+            case R.id.webview_btn2:
+                mWebview.loadUrl("http://m.sohu.com/");
+                break;
+            case R.id.webview_btn3:
+                mWebview.loadUrl("https://weibo.com/u/2745871101/home");
+                break;
+            case R.id.webview_btn4:
+                mWebview.loadUrl("https://m.mi.com/");
+                break;
+        }
     }
 }
